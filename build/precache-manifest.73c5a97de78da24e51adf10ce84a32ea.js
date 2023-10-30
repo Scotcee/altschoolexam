c5a3a784b935fab1b7360ddf9c01bb7b6f4d099c
Scotcee <amaryachuks@gmail.com>
2023-10-30 06:26:33 +0100

    build changes

---
+++ build/precache-manifest.73c5a97de78da24e51adf10ce84a32ea.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c4d577fecafe55d175e207db8ddda3f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "798d8b86f644c8bdccbb",
+    "url": "/static/css/main.8402e935.chunk.css"
+  },
+  {
+    "revision": "8caa18f40875f08f03fe",
+    "url": "/static/js/2.d27ff8e4.chunk.js"
+  },
+  {
+    "revision": "0792de7b061135c911e3f2b2d9fc8dcb",
+    "url": "/static/js/2.d27ff8e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "798d8b86f644c8bdccbb",
+    "url": "/static/js/main.a666ec7d.chunk.js"
+  },
+  {
+    "revision": "e2dd0374a34d4bf564ae",
+    "url": "/static/js/runtime-main.5dc28ab3.js"
+  },
+  {
+    "revision": "6395a625ac95b87f8dd42c17e37bfb14",
+    "url": "/static/media/404image.6395a625.jpeg"
+  }
+]);
